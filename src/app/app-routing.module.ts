604fbf7055bf3258b164d1c154106f562ab28452
Christopher Esparza <christopher.esparza@hotmail.com>
2023-04-26 16:18:19 -0600

    nuevo componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
+import { SectorAgricolaComponent } from './sector-agricola/sector-agricola.component';
+
 const routes: Routes = [
   {path: '', redirectTo: '/inicio', pathMatch: 'full'}, //Redirigir en ruta vacía
-  {path: 'inicio', component:InicioComponent}
+  {path: 'inicio', component:InicioComponent},
+  {path: 'sectoragro', component:SectorAgricolaComponent}
 ];
 
 @NgModule({
---
+++ src/app/sector-agricola/sector-agricola.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sector-agricola',
+  templateUrl: './sector-agricola.component.html',
+  styleUrls: ['./sector-agricola.component.scss']
+})
+export class SectorAgricolaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
