0acd87927e4b689be5ac4ec2f9c56c5a9dfbbcd7
Sergio12332 <tork_sergio@outlook.es>
2023-04-26 16:50:23 -0600

    imagen sector urbano

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { SectorAgricolaComponent } from './sector-agricola/sector-agricola.component';
 import { SectorUrbanoComponent } from './sector-urbano/sector-urbano.component';
+import { CuidadoAnimalComponent } from './cuidado-animal/cuidado-animal.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/inicio', pathMatch: 'full'}, //Redirigir en ruta vacía
   {path: 'inicio', component:InicioComponent},
   {path: 'sectoragro', component:SectorAgricolaComponent},
+  {path: 'cuidadoAnimal',component:CuidadoAnimalComponent},
   {path: 'SectorUrbano', component:SectorUrbanoComponent}
 ];
 
