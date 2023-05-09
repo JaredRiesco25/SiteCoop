d9f9c6df57dc6c7b0dfe026dd4539bcacc3e39b1
Sergio12332 <tork_sergio@outlook.es>
2023-05-09 17:57:20 -0600

    conexion de api con imagenes y texto

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -7,17 +7,29 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent implements OnInit {
-
-  currentIndex = 0;
+  imagenBase64!: string;
+  ImageUrl!: any;
+  titulo!:string;
+  Subtitulo!:string;
+  texto!:string;
   constructor(private http: HttpClient) {
 
-    this.http.get('http://WS_TiendaCoop.innovativepartner.tech//Guardar/ObtenFavoritos/88').subscribe(data => {
-  console.log(data);
-});
-
+/*   this.http.get('http://WS_TiendaCoop.innovativepartner.tech//Guardar/ObtenFavoritos/88').subscribe(data => {
+  this.resultado=data;
+}
+); */
+ 
    }
 
   ngOnInit(): void {
+    this.http.get('http://WS_TiendaCoop.innovativepartner.tech//Guardar/ObtenFavoritos/88').subscribe(response => {
+      this.ImageUrl=response;
+      this.imagenBase64= 'data:image/png;base64,'+this.ImageUrl[0].Imagen;
+      this.titulo = this.ImageUrl[0].Nombre;
+      this.Subtitulo = this.ImageUrl[0].Clave;
+      this.texto = this.ImageUrl[0].Precio;
 
+    }
+    );  
   }
 }
