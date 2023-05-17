03142c75c99104d072001119dbdbc80f4447a038
Sergio12332 <tork_sergio@outlook.es>
2023-05-16 19:51:42 -0600

    ajuste de responsivo en nav

--- src/app/servicios/servicios.component.ts
+++ src/app/servicios/servicios.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, Renderer2 ,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servicios',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servicios.component.scss']
 })
 export class ServiciosComponent implements OnInit {
+  scrollingDown = false;
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
+  }
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const offset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    this.scrollingDown = offset > 0; // Cambia el valor para controlar cuándo se activa el efecto
   }
 
 }
