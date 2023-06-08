515463691662bd39aff19c79d24adb3f37248828
Sergio12332 <tork_sergio@outlook.es>
2023-06-07 18:40:43 -0600

    cambio de video por imagenes en inicial

--- src/app/inicial/inicial.component.ts
+++ src/app/inicial/inicial.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit,ElementRef  } from '@angular/core';
-import { OrientacionService  } from '../orientacion.service';
+import { Component, OnInit } from '@angular/core';
+
 @Component({
   selector: 'app-inicial',
   templateUrl: './inicial.component.html',
@@ -7,37 +7,21 @@ import { OrientacionService  } from '../orientacion.service';
 })
 export class InicialComponent implements OnInit {
   currentIndex = 0;
-  videos: string[] = ['../../assets/Videos/carrusel11.mp4', '../../assets/Videos/carrusel22.mp4', '../../assets/Videos/carrusel33.mp4', '../../assets/Videos/carrusel44.mp4'];
-  constructor(private elRef: ElementRef,private orientationService: OrientacionService) { }
-  videoSeleccionado: number = this.currentIndex;
 
-  cambiarVideo(): void {
-    const videoElement = document.getElementById('miVideo') as HTMLVideoElement;
-    console.log(this.currentIndex);
-
-    videoElement.src = `assets/${this.videos[this.currentIndex]}`;
-    videoElement.load();
-    videoElement.play();
-  }
+  constructor() { }
 
   ngOnInit(): void {
-    this.orientationService.lockOrientation();
-
-    setInterval(() => {
-      this.cambiarVideo();
+      setInterval(() => {
       this.currentIndex = (this.currentIndex + 1) % 4;
-    }, 8100);;
+      console.log(this.currentIndex);
+    }, 5000);
   }
-
   prevslide(): void {
     this.currentIndex = (this.currentIndex === 0) ? 3 : this.currentIndex - 1;
-
-    this.cambiarVideo();
   }
   
   nextslide(): void {
     this.currentIndex = (this.currentIndex === 3) ? 0 : this.currentIndex + 1;
-    
-    this.cambiarVideo();
   }
+
 }
